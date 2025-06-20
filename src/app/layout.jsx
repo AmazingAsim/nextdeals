ff56721a92f272cb6c1916a6d394d230d8da4230
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2025-06-20 15:03:05 +0530

    corredtion to google anylatics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import '@/lib/fontawesome';
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 import dynamic from 'next/dynamic';
@@ -22,14 +23,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
         <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-QK2R5LW2W1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QK2R5LW2W1');
+            `,
+          }}
+        />
 
-  gtag('config', 'G-QK2R5LW2W1');
-</script>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-QK2R5LW2W1`}
+          strategy="afterInteractive"
+        />
+      </head>
+
       <body className={inter.className}>
         <BootstrapClientLoader />
         <Navbar />
