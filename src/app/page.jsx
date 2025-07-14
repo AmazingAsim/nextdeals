0673b416c48823eaf0d3a3c2269293ee46174b05
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2025-07-15 01:53:55 +0530

    stuff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,26 @@ import styles from "./page.module.css";
 import Carousel from "@/components/Carousel";
 import FeaturedProducts from "@/components/FeaturedProducts";
 
+export async function generateMetadata({ params }) {
+  return {
+    title: `Deals of America || Deals from america, coupon codes for amazon`,
+    description: `latest deals at amazon,wallmart and more`,
+    icons: {
+      icon: "/logo.png"
+    },
+     alternates: {
+      canonical: "https://dealsfromamerica.com",
+    }
+  }
+}
+
 export default function Home() {
   return (
     <main>
       <Carousel/>
       <div className="container">
       <h1 className="text-center mt-1 fs-6 main-header text-primary">
-        Best Deals in America & Discounts on Amazon,Walmart,Target,  on Latest Products like Phones, Laptops, Watches, Fashion, Electronics, Home Appliances
+      Deals of America || Deals from america, coupon codes for amazon
       </h1>
       </div>
       <FeaturedProducts/>
